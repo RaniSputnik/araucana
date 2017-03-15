507f04053e47f0f0d82340ee79276338f66ec6f6
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-03-15 14:50:33 +1300

    [RED] Test basic site is scraped

--- scrape/scape_test.go
+++ scrape/scape_test.go
@@ -1,6 +1,8 @@
 package scrape_test
 
 import (
+	"log"
+	"net/http"
 	"testing"
 
 	. "github.com/RaniSputnik/araucana/scrape"
@@ -18,6 +20,85 @@ func TestScrapeThrowsErrorWhenURLInvalid(t *testing.T) {
 		_, err := Site(url)
 		if err != expected {
 			t.Errorf("Expected '%s' but got '%v' for url '%s'", expected, err, url)
+		}
+	}
+}
+
+func TestTargetSiteIsSinglePage(t *testing.T) {
+	addr := ":3000"
+	srv := setupStaticFileServer("test/basic", addr)
+	defer srv.Close()
+
+	expected := &Sitemap{
+		XMLNS: SitemapXMLNamespace,
+		URLSet: []*SitemapURL{
+			&SitemapURL{
+				Loc: "http://localhost:3000/index.html",
+			},
+		},
+	}
+	sitemap, err := Site("http://localhost:3000/index.html")
+
+	if err != nil {
+		t.Errorf("Expected no error but got '%v'", err)
+	}
+	ensureSitemapsMatch(t, sitemap, expected)
+}
+
+func setupStaticFileServer(dir string, addr string) *http.Server {
+	srv := &http.Server{
+		Handler: http.FileServer(http.Dir(dir)),
+		Addr:    addr,
+	}
+
+	go func(srv *http.Server) {
+		log.Fatal(srv.ListenAndServe())
+	}(srv)
+
+	return srv
+}
+
+func ensureSitemapsMatch(t *testing.T, got *Sitemap, expected *Sitemap) {
+	// Handle nil cases
+	if got == nil {
+		if expected != nil {
+			t.Fatalf("Expected valid sitemap but got 'nil'")
+		} else {
+			// Both got & expected are nil
+			return
+		}
+	}
+
+	if len(expected.URLSet) != len(got.URLSet) {
+		t.Errorf("Expected %d urls, got %d urls", len(expected.URLSet), len(got.URLSet))
+	}
+
+	// TODO these comparrissons could be done a lot faster
+	// lets just KISS for now
+
+	for _, url := range got.URLSet {
+		found := false
+		for _, test := range expected.URLSet {
+			if url.Loc == test.Loc {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Got unexpected url '%s'", url.Loc)
+		}
+	}
+
+	for _, test := range expected.URLSet {
+		found := false
+		for _, url := range got.URLSet {
+			if url.Loc == test.Loc {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Expected url '%s' was not found", test.Loc)
 		}
 	}
 }
