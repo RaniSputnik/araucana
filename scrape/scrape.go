8dd8d4d4c3dd4d134b69a94d0a8930827495c128
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-03-17 01:56:30 +1300

    [R#] First pass at concurrency

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -21,26 +21,59 @@ func (s *scraper) Scrape(addr string) error {
 	s.results[addr] = thisPage
 	s.logger.Printf("Scraping %s", addr)
 
+	urlsChan := make(chan []string)
+	errorChan := make(chan error)
+	go s.doScrape(thisPage, urlsChan, errorChan)
+
+	inflight := 1
+	for inflight > 0 {
+		inflight--
+		select {
+		case nextURLs := <-urlsChan:
+			for _, nextURL := range nextURLs {
+				if _, done := s.results[nextURL]; !done {
+					nextPage := &Page{nextURL, []*Asset{}}
+					s.results[nextURL] = nextPage
+					inflight++
+					go s.doScrape(nextPage, urlsChan, errorChan)
+				} else {
+					s.logger.Printf("We've already scraped '%s'", nextURL)
+				}
+			}
+		case err := <-errorChan:
+			// TODO drain the doScrape goroutines waiting to publish to urlsChan
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *scraper) doScrape(page *Page, urlsChan chan<- []string, errorChan chan<- error) {
 	// TODO never ever use the default client in production
-	response, err := http.Get(addr)
+	response, err := http.Get(page.URL)
 	if err != nil {
-		return ErrHTTPError
+		errorChan <- ErrHTTPError
 	}
 
 	if httpStatusIsError(response.StatusCode) {
-		return ErrHTTPError
+		errorChan <- ErrHTTPError
 	}
 
 	doc, err := html.Parse(response.Body)
 	if err != nil {
 		// TODO return defined error
-		return err
+		errorChan <- err
 	}
 
+	nextURLBatch := []string{}
 	var f func(*html.Node)
 	f = func(n *html.Node) {
-		s.tryFollowLinkIfExistsInNode(n)
-		s.tryAddAsset(n, thisPage)
+		if nextURL := s.getLinkIfExistsInNode(n); nextURL != "" {
+			nextURLBatch = append(nextURLBatch, nextURL)
+		} else {
+			s.tryAddAsset(n, page)
+		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
 		}
@@ -51,39 +84,32 @@ func (s *scraper) Scrape(addr string) error {
 	// That could be a neat feature - find when you have duplicate
 	// links to a single resource
 
-	return nil
+	urlsChan <- nextURLBatch
 }
 
-func (s *scraper) tryFollowLinkIfExistsInNode(n *html.Node) {
+func (s *scraper) getLinkIfExistsInNode(n *html.Node) string {
 	if n.Type != html.ElementNode || n.Data != "a" {
 		// Skip this node, it's not an <a> tag
-		return
+		return ""
 	}
 	ok, href := attr(n, "href")
 	if !ok {
 		s.logger.Printf("<a> tag appears to have no 'href' attribute")
-		return
+		return ""
 	}
 
 	parsedHref, err := s.GetFullURLWithoutHashAndQuery(href)
 	if err != nil {
 		s.logger.Printf("<a> tag has a href attribute (%s) we can't parse: '%v'", href, err)
-		return
+		return ""
 	}
 
 	if parsedHref.Host != s.rootURL.Host {
 		s.logger.Printf("External link will not be followed '%s'", href)
-		return
+		return ""
 	}
 
-	href = parsedHref.String()
-	if _, ok := s.results[href]; ok {
-		s.logger.Printf("We've already scraped '%s'", href)
-		return
-	}
-
-	// All the checks have run, we can safely scrape now
-	s.Scrape(href)
+	return parsedHref.String()
 }
 
 // tryAddAsset will check if the node is an asset reference
