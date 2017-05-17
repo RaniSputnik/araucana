af49362a66110ec87dc4f732177e668336b5e05a
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-05-17 17:28:10 +0100

    Update tests to use http test package

--- scrape/scape_test.go
+++ scrape/scape_test.go
@@ -3,6 +3,8 @@ package scrape_test
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	. "github.com/RaniSputnik/araucana/scrape"
@@ -25,18 +27,17 @@ func TestScrapeThrowsErrorWhenURLInvalid(t *testing.T) {
 }
 
 func TestTargetSiteIsSinglePage(t *testing.T) {
-	addr := ":3000"
-	srv := test.SetupStaticFileServer("test/basic", addr)
+	srv := setupStaticFileServer("test/basic")
 	defer srv.Close()
 
 	expected := &Sitemap{
 		Pages: []*Page{
 			&Page{
-				URL: "http://localhost:3000/index.html",
+				URL: srv.URL + "/index.html",
 			},
 		},
 	}
-	sitemap, err := Site(context.Background(), "http://localhost:3000/index.html")
+	sitemap, err := Site(context.Background(), srv.URL+"/index.html")
 
 	if err != nil {
 		t.Errorf("Expected no error but got '%v'", err)
@@ -45,23 +46,22 @@ func TestTargetSiteIsSinglePage(t *testing.T) {
 }
 
 func TestLinksAreAlsoScraped(t *testing.T) {
-	addr := ":3000"
-	srv := test.SetupStaticFileServer("test/basic2", addr)
+	srv := setupStaticFileServer("test/basic2")
 	defer srv.Close()
 
 	expected := &Sitemap{
 		Pages: []*Page{
 			&Page{
-				URL:   "http://localhost:3000/index.html",
-				Pages: []string{"http://localhost:3000/contact.html"},
+				URL:   srv.URL + "/index.html",
+				Pages: []string{srv.URL + "/contact.html"},
 			},
 			&Page{
-				URL:   "http://localhost:3000/contact.html",
-				Pages: []string{"http://localhost:3000/index.html"},
+				URL:   srv.URL + "/contact.html",
+				Pages: []string{srv.URL + "/index.html"},
 			},
 		},
 	}
-	sitemap, err := Site(context.Background(), "http://localhost:3000/index.html")
+	sitemap, err := Site(context.Background(), srv.URL+"/index.html")
 
 	if err != nil {
 		t.Errorf("Expected no error but got '%v'", err)
@@ -70,18 +70,17 @@ func TestLinksAreAlsoScraped(t *testing.T) {
 }
 
 func TestExternalLinksAreNotScraped(t *testing.T) {
-	addr := ":3000"
-	srv := test.SetupStaticFileServer("test/external", addr)
+	srv := setupStaticFileServer("test/external")
 	defer srv.Close()
 
 	expected := &Sitemap{
 		Pages: []*Page{
 			&Page{
-				URL: "http://localhost:3000/index.html",
+				URL: srv.URL + "/index.html",
 			},
 		},
 	}
-	sitemap, err := Site(context.Background(), "http://localhost:3000/index.html")
+	sitemap, err := Site(context.Background(), srv.URL+"/index.html")
 
 	if err != nil {
 		t.Errorf("Expected no error but got '%v'", err)
@@ -90,22 +89,21 @@ func TestExternalLinksAreNotScraped(t *testing.T) {
 }
 
 func TestHashAndQueryStringAreIgnored(t *testing.T) {
-	addr := ":3000"
-	srv := test.SetupStaticFileServer("test/basic3", addr)
+	srv := setupStaticFileServer("test/basic3")
 	defer srv.Close()
 
 	expected := &Sitemap{
 		Pages: []*Page{
 			&Page{
-				URL:   "http://localhost:3000/index.html",
-				Pages: []string{"http://localhost:3000/contact.html"},
+				URL:   srv.URL + "/index.html",
+				Pages: []string{srv.URL + "/contact.html"},
 			},
 			&Page{
-				URL: "http://localhost:3000/contact.html",
+				URL: srv.URL + "/contact.html",
 			},
 		},
 	}
-	sitemap, err := Site(context.Background(), "http://localhost:3000/index.html")
+	sitemap, err := Site(context.Background(), srv.URL+"/index.html")
 
 	if err != nil {
 		t.Errorf("Expected no error but got '%v'", err)
@@ -114,37 +112,35 @@ func TestHashAndQueryStringAreIgnored(t *testing.T) {
 }
 
 func TestAssetReferencesAreIncluded(t *testing.T) {
-	addr := ":3000"
-	srv := test.SetupStaticFileServer("test/assets", addr)
+	srv := setupStaticFileServer("test/assets")
 	defer srv.Close()
 
-	site := "http://localhost:3000"
 	expected := &Sitemap{
 		Pages: []*Page{
 			&Page{
-				URL: fmt.Sprintf("%s/index.html", site),
+				URL: fmt.Sprintf("%s/index.html", srv.URL),
 				Assets: []*Asset{
 					&Asset{
 						Type: AssetTypeImage,
-						URL:  fmt.Sprintf("%s/hello-world.jpg", site),
+						URL:  fmt.Sprintf("%s/hello-world.jpg", srv.URL),
 					},
 					&Asset{
 						Type: AssetTypeLink,
-						URL:  fmt.Sprintf("%s/index.css", site),
+						URL:  fmt.Sprintf("%s/index.css", srv.URL),
 					},
 					&Asset{
 						Type: AssetTypeLink,
-						URL:  fmt.Sprintf("%s/favicon.ico", site),
+						URL:  fmt.Sprintf("%s/favicon.ico", srv.URL),
 					},
 					&Asset{
 						Type: AssetTypeScript,
-						URL:  fmt.Sprintf("%s/index.js", site),
+						URL:  fmt.Sprintf("%s/index.js", srv.URL),
 					},
 				},
 			},
 		},
 	}
-	sitemap, err := Site(context.Background(), fmt.Sprintf("%s/index.html", site))
+	sitemap, err := Site(context.Background(), fmt.Sprintf("%s/index.html", srv.URL))
 
 	if err != nil {
 		t.Errorf("Expected no error but got '%v'", err)
@@ -153,11 +149,10 @@ func TestAssetReferencesAreIncluded(t *testing.T) {
 }
 
 func TestTargetSite404ResultsInError(t *testing.T) {
-	addr := ":3000"
-	srv := test.SetupStaticFileServer("test/basic", addr)
+	srv := setupStaticFileServer("test/basic")
 	defer srv.Close()
 
-	_, err := Site(context.Background(), "http://localhost:3000/doesnotexist.html")
+	_, err := Site(context.Background(), srv.URL+"/doesnotexist.html")
 
 	if err != ErrHTTPError {
 		t.Errorf("Expected '%v' but got '%v'", ErrHTTPError, err)
@@ -171,4 +166,8 @@ func TestConnectionTroubleResultsInError(t *testing.T) {
 	if err != ErrHTTPError {
 		t.Errorf("Expected '%v' but got '%v'", ErrHTTPError, err)
 	}
+}
+
+func setupStaticFileServer(dir string) *httptest.Server {
+	return httptest.NewServer(http.FileServer(http.Dir(dir)))
 }
